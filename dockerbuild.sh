cada3414edba75e53f121cbc4baa0906940d6bf4
Paul Datta <pauldatta@microsoft.com>
2023-03-30 21:52:00 +0800

    POC: Sample Dockerfile for this to get containerized
        * Copy Dockerfile.sample as Dockerfile
        * Enter values for ENV variables
        * You can run dockerbuild.sh if you use AZD
    
        TODO: Change how the ENVs are delivered

---
+++ dockerbuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Build
+docker build -t searchgpt .
+
+# Extract environment variables from Azure DevOps
+ENV_VARS=$(azd env get-values)
+
+# Parse the output and set each variable using the --env option
+echo "$ENV_VARS" | while IFS='=' read -r key value; do
+    value=$(echo "$value" | sed 's/^"//' | sed 's/"$//')
+    docker_run_opts+=" --env $key=$value"
+done
+
+# Run the Docker container with the environment variables
+docker run $docker_run_opts -p 5000:5000 searchgpt
